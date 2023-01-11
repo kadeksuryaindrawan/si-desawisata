972190d122cec430139a7a2f2fae90de2d4df01b
kadeksuryaindrawan <kadeksuryaindrawan@gmail.com>
2023-01-11 12:01:55 +0800

    :smile: new-role

---
+++ app/Http/Controllers/LandingController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LandingController extends Controller
+{
+    public function index(){
+        return view('index');
+    }
+
+    public function about(){
+        return view('about');
+    }
+
+    public function destination(){
+        return view('destination');
+    }
+
+    public function contact(){
+        return view('contact');
+    }
+}
